9b492d3ea3d838160e0d93449feb07c5937c6749
Evan <newmanrevan@gmail.com>
2017-07-10 19:15:08 -0700

    added the player inventory struct

---
+++ struct.h
@@ -0,0 +1,30 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+
+struct pokemon_trainer {
+
+    int pokeballs;
+    int stones;
+
+    int charmander;
+    int charmeleon;
+    int charizard;
+    int torchic;
+    int combuskin;
+    int blaziken;
+    int squirtle;
+    int wartortle;
+    int blastoise;
+    int mudkip;
+    int marshtomp;
+    int swampert;
+    int bulbasaur;
+    int ivysaur;
+    int venusaur;
+    int treecko;
+    int grovyle;
+    int sceptile;
+
+};
+
+#endif
